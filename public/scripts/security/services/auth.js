fd136ad16719d5ad05f299d5064b4d5cffae8dcb
Angel L Villalain-Garcia <avillalain@softekpr.com>
2014-03-29 19:47:19 -0400

    Initial login support

--- public/scripts/security/services/auth.js
+++ public/scripts/security/services/auth.js
@@ -3,17 +3,19 @@
 
     angular.module('find-the-dude')
         .factory('$auth', function($facebook, ipCookie, $q, $location, $rootScope) {
+            $rootScope.user = ipCookie('user');
+
             var login = function(response) {
                 if(response.status === 'connected') {
                     ipCookie('user', response, {expires: response.authResponse.expiresIn/60, expirationUnit: 'minutes'});
                     $location.path('/dudes');
+                    $rootScope.user = ipCookie('user');
                 }
                 return $q.reject(response);
             };
 
             return {
                 isLoggedIn: function() {
-                    $rootScope.user = ipCookie('user');
                     return $rootScope.user !== undefined;
                 },
                 login: function() {
