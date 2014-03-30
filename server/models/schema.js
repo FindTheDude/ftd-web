07007df5abb374d954caaf9b29b3564ba12d705b
Andrés Colón <andres@thoughtware.tv>
2014-03-29 23:15:20 -0400

    Moving models to server directory per-request

---
+++ server/models/schema.js
@@ -0,0 +1,12 @@
+// File that defines our User Model
+var mongoose = require('mongoose');
+var Schema = mongoose.Schema;
+var userSchema = new Schema({
+  fullName:  String,
+  facebookId: String,
+  accessToken: String
+});
+
+
+var User = mongoose.model('User', userSchema);
+module.export = User
--- server/server.js
+++ server/server.js
@@ -15,7 +15,7 @@
     //     // yay!
     // });
 
-    var User = require('../models/schema.js');
+    var User = require('models/schema.js');
 
     app.get('/user/get', function(req, res){
       var user = new User({ fullname: 'Andres', facebookId: 'thisismyfacebookid', accessToken: 'what_a_token!'});
