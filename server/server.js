6dc076be320e260678612edf4d1a33d4fb217017
Angel L Villalain-Garcia <avillalain@softekpr.com>
2014-03-29 23:07:46 -0400

    Initial server implementation

--- server/server.js
+++ server/server.js
@@ -2,12 +2,11 @@
     'use strict';
 
     var express = require('express'),
-        path = require('path');
-
+        path = require('path'),
+        logging = require('./logging'),
+        mongoose = require('mongoose');
     var app = express();
 
-    // DB Connection
-    var mongoose = require('mongoose');
     mongoose.connect('mongodb://localhost/ftdapp');
     // var db = mongoose.createConnection('localhost', 'FTDApp');
     // db.on('error', console.error.bind(console, 'connection error:'));
@@ -23,19 +22,22 @@
       res.send('Creating the User');
     });
 
+    var configuration = require('./configuration');
+
     console.log(__dirname);
     //this is needed only on dev environment
     //also avoid hard coding the value
     app.use(require('connect-livereload')({
-        port: 35729
+        port: configuration.get('livereload:port')
     }));
 
     app.use(express.static(path.join(__dirname, '../build')));
     app.use(express.static(path.join(__dirname, '../public')));
     app.use(express.errorHandler());
+    app.use(logging);
 
-    app.listen(process.env.PORT || 3000, function() {
-        console.log('Express server listening on port 3000');
+    app.listen(configuration.get('express:port'), function() {
+        console.log('Express server listening on port ' + configuration.get('express:port'));
     });
 
     module.exports = app;
