abb6be6ccb8edf9461707cd265726b88f083cb1c
Angel L Villalain-Garcia <avillalain@softekpr.com>
2014-04-04 15:57:26 -0400

    cleanup of code

--- server/lib/facebook/index.js
+++ server/lib/facebook/index.js
@@ -6,47 +6,77 @@
     var configuration = require('../configuration');
 
     var Facebook = function(clientId, clientSecret) {
-        this.clientId = clientId;
-        this.clientSecret = clientSecret;
+        var clientId = clientId;
+        var clientSecret = clientSecret;
+        var baseUrl = 'https://graph.facebook.com';
 
-        this._get = function(url, options, callback) {
-            var request = restler.get(url, options);
+        var _get = function(path, options, callback) {
+            var request = restler.get(baseUrl + path, options);
             for(var event in callback) {
                 if(callback.hasOwnProperty(event)) {
                     request.on(event, callback[event]);
                 }
             }
         };
-    };
 
-    Facebook.prototype.retrieveLongLiveToken = function(accessToken, callback) {
-        if('function' === typeof callback) {
-            callback['complete'] = callback;
-        }
-        var options = {
-            query: {
-                'grant_type': 'fb_exchange_token',
-                'client_id': this.clientId,
-                'client_secret': this.clientSecret,
-                'fb_exchange_token': accessToken
+        this.retrieveLongLiveToken = function(accessToken, callback) {
+            if('function' === typeof callback) {
+                callback['complete'] = callback;
             }
-        };
-        this._get('https://graph.facebook.com/oauth/access_token', options, callback);
-    };
+            var options = {
+                query: {
+                    'grant_type': 'fb_exchange_token',
+                    'client_id': clientId,
+                    'client_secret': clientSecret,
+                    'fb_exchange_token': accessToken
+                }
+            };
 
-    Facebook.prototype.me = function(accessToken, callback) {
-        if('function' === typeof callback) {
-            callback['complete'] = callback;
-        }
-
-        var options = {
-            query: {
-                'access_token': accessToken,
-                'fields': 'id,name'
-            }
+            _get('/oauth/access_token', options, callback);
         };
 
-        this._get('https://graph.facebook.com/me', options, callback);
+        this.me = function(accessToken, callback) {
+            if('function' === typeof callback) {
+                callback['complete'] = callback;
+            }
+
+            var options = {
+                query: {
+                    'access_token': accessToken,
+                    'fields': 'id,name'
+                }
+            };
+
+            _get('/me', options, callback);
+        };
+
+        this.friends = function(accessToken, callback) {
+            if('function' === typeof callback) {
+                callback['complete'] = callback;
+            }
+
+            var options = {
+                query: {
+                    'access_token': accessToken
+                }
+            };
+
+            _get('/me/friends', options, callback);
+        };
+
+        this.photos = function(accessToken, userId, callback) {
+            if('function' === typeof callback) {
+                callback['complete'] = callback;
+            }
+
+            var options = {
+                query: {
+                    'access_token': accessToken
+                }
+            };
+
+            _get('/' + userId + '/photos', options, callback);
+        };
     };
 
     module.exports = new Facebook(configuration.get('facebook:client_id'),
